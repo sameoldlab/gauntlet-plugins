fdb5de330486625bcde28e27f6e531dc4a8a2c2c
ibro <sameoldlab@proton.me>
2025-08-24 22:39:53 +0100

    wlrctl opened windows

--- src/windows.tsx
+++ src/windows.tsx
@@ -1,38 +1,46 @@
 import { ReactElement, useState } from "react";
-import { List, TextAccessory } from "@project-gauntlet/api/components";
+import { Action, ActionPanel, Inline, List, TextAccessory } from "@project-gauntlet/api/components";
 
-export default function SearchBarExample(): ReactElement {
-  const pop = new PopLauncherClient()
-  pop.connect();
+export default function(): ReactElement {
   const [searchText, setSearchText] = useState<string | undefined>("");
-  const [results, setResults] = useState<SearchResult[]>([])
+  const [results, setResults] = useState<{ id: string, title: string }[]>([])
+  const dec = new TextDecoder()
 
-  return (
-    <List>
-      <List.SearchBar placeholder="What knowledge do you seek...?"
-        value={searchText}
-        onChange={async (value) => {
-          const results = await pop.search(value ?? '');
-          console.log(results)
-          setResults(results)
-
-          setSearchText(value)
-        }}
-      />
-      {results
-        // .filter(value => !searchText ? true : value.toLowerCase().includes(searchText))
-        .map(value => (
-          <List.Item subtitle={value.description} id={value.id.toString()} title={value.name}
-            accessories={[
-              <TextAccessory text={value.icon?.Name || value.icon?.Mime || ''} />
-            ]}
-          />
-        ))
-      }
-    </List>
-  )
-}
-
-
+  new Deno.Command('wlrctl', {
+    args: ["toplevel", "list"]
+  }).output().then((cmd) => {
+    if (!cmd.success) {
+      let err = dec.decode(cmd.stderr)
+      console.log(err)
+      return
     }
+
+    let output = dec.decode(cmd.stdout).trim().split('\n').map(line => {
+      const [id, title] = line.split(': ')
+      return { id, title }
+    })
+    setResults(output)
+  })
+
+  const select = (title: string) => new Deno.Command('wlrctl', {
+    args: ["toplevel", "focus", `title:${title}`]
+  }).spawn()
+
+  return (<List
+    actions={<ActionPanel>
+      <Action label="Focus" onAction={(id) => {
+        if (id) select(id)
+      }} />
+    </ActionPanel>}
+  >
+    <List.SearchBar
+      value={searchText}
+      onChange={async (value) => {
+        setSearchText(value)
+      }}
+    />
+    {results.map(app => (
+      <List.Item id={app.title} title={app.title} subtitle={app.id} />
+    ))}
+  </List>)
 }
